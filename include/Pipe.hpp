12674f67cc6ece3005175b5fabc348a6fea0f8f8
VolatileBits <soumik20020906@gmail.com>
2023-06-18 16:38:01 +0530

    Added pipes

---
+++ include/Pipe.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "Entity.hpp"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <cstdio>
+#include <vector>
+
+class Pipe:public Entity
+{
+	float pos_x,pos_y;
+public:
+	Pipe(SDL_Texture* p_tex,float x);
+};
+
+
+void check_pipe(std::vector<Pipe> &pipes,int offset);
